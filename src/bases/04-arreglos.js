11dfc5cfb2af9612e56ba87438d8b9b6a5f695c6
mgomez <mariajosegomezcampo@gmail.com>
2022-04-03 13:03:39 -0400

    first commit

---
+++ src/bases/04-arreglos.js
@@ -0,0 +1,27 @@
+
+
+// Arreglos en JS
+// const arreglo = new Array( 100 );
+const arreglo = [1,2,3,4];
+// arreglo.push(1)
+// arreglo.push(2)
+// arreglo.push(3)
+// arreglo.push(4)
+
+let arreglo2 = [ ...arreglo, 5 ]; // OPERADOR EXPRES ... hace copia del arreglo anterior y agrega el 5 
+
+const arreglo3 = arreglo2.map( function(numero) {
+    return numero * 2;
+});
+
+
+console.log( arreglo );
+console.log( arreglo2 );
+console.log( arreglo3 );
+
+
+
+
+
+
+
